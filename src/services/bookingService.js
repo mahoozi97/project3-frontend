7a2ebc517ae0441336f94f074f36f5fda6705ce0
mahoozi97 <mahoozi97@icloud.com>
2026-04-18 13:35:39 +0300

    add booking service with CRUD operations

---
+++ src/services/bookingService.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+const newBooking = async (token, data) => {
+  await axios.post(`${BASE_URL}/booking`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+const fetchAllBooking = async (token) => {
+  const res = await axios.get(`${BASE_URL}/booking`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+const fetchAllBookingByUserId = async (token) => {
+  const res = await axios.get(`${BASE_URL}/booking/my-booking`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+const fetchBookingById = async (token, id) => {
+  const res = await axios.get(`${BASE_URL}/booking/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return res.data;
+};
+
+const editBooking = async (token, id, data) => {
+  await axios.put(`${BASE_URL}/booking/${id}`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+const deleteBooking = async (token, id) => {
+  await axios.delete(`${BASE_URL}/booking/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+
+export {
+  newBooking,
+  fetchAllBooking,
+  fetchAllBookingByUserId,
+  fetchBookingById,
+  editBooking,
+  deleteBooking,
+};
