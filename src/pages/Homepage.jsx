cb354add42acd3eaf4ad6deade50a74326600275
HusainAlmisbah <husain.almisbah@gmail.com>
2026-04-21 19:36:09 +0300

    Images Added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router'
+import bookNowImg from '../assets/Book-Now.png'
+import readMoreImg from '../assets/Read More.png'
 
-function Homepage() {
+function Homepage() { 
   return (
     <div>
-      <h1 className='homepage'>Welcome to our website</h1>
-      <p>Discover our amazing products and services</p>
+      <br />
+      <h1 className='homepage'>Welcome to KhalijiGo</h1>
+      <p>استمتع بتجربة مميزة مع خدماتنا</p>
 
       <br /> <br />
+      <img src={bookNowImg} alt="Book Now" />
+      <br /> <br />
       <Link to="/book-now"><button className='btn'>Book Now</button></Link>
+      <br /> <br /> <br /> <br /> <br />
+      <img src={readMoreImg} alt="Read More" />
       <br /> <br />
       <Link to="/blogs"><button className='btn'>Read more</button></Link>
     </div>  
   )
 }
-
 export default Homepage
