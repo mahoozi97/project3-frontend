d61b52772e9fdf44cee58e552f685acdff7dffde
ahazeem <ahmedalihazeem24@gmail.com>
2026-04-21 15:47:59 +0300

    Did the Home.jsx for the blogs

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,22 +1,17 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = "http://localhost:5000/api/blogs";
 
-const newBlog = async (token, data) => {
-    await axios.post(`${BASE_URL}/api/blog`, data, {
-        headers: { Authorization: `Bearer ${token}`},
-    })
+const authHeader = () => {
+    const token = localStorage.getItem("token");
+    return { headers: { Authorization: `Bearer ${token}` } };
 }
 
-const getAllBlogs = async (token) => {
-    const res = await axios.get(`${BASE_URL}/api/blog`, {
-        headers: { Authorization: `Bearer ${token}`},
-    })
-    return res.data
-} 
- const getASingleBlogByUserId = async (token) => {
-    const res = await.get(`${BASE_URL}/api/blog/:id`, {
-        headers: { Authorization: `Bearer ${token}`}, 
-    })
-    return res.data
- }
+export const getAllBlogs = () => axios.get(`${API_URL}`);
+export const getBlogById = (id) => axios.get(`${API_URL}/${id}`);
+export const createBlog = (blogData) => axios.post(`${API_URL}`, blogData, authHeader());
+export const updateBlog = (id, blogData) => axios.put(`${API_URL}/${id}`, blogData, authHeader());
+export const deleteBlog = (id) => axios.delete(`${API_URL}/${id}`, authHeader());
+
+export const addComment = (blogId, commentData) => axios.post(`${API_URL}/${blogId}/comments`, commentData, authHeader());
+export const deleteComment = (blogId, commentId) => axios.delete(`${API_URL}/${blogId}/comments/${commentId}`, authHeader());
